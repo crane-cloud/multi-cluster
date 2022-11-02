e898e5b90116719c80614e05fc0a80ecce37e1ee
Henry Mutegeki <hmutegeki@Henrys-MacBook-Pro.local>
2022-11-02 08:59:52 +0300

    initial rpc setup

---
+++ client.py
@@ -0,0 +1,45 @@
+from jsonrpclib import Server
+import sys
+from datetime import datetime
+
+cluster_list = [{"id":1, "cluster_name":"cis-lib1", "ip": "http://localhost:5141"}, 
+                    {"id":2, "cluster_name":"dicts-01", "ip": "http://localhost:5142"}]
+
+def main():
+    server = Server('http://localhost:5141')
+    
+    
+    try:
+        # INSERT NETWORK METRICS
+        cluster_id= 1
+        throughput = 2.233
+        latency = 4.922
+        jitter = 1.59229
+        date = '28-10-2022'
+        metrics = (cluster_id,throughput,latency,jitter,date)
+        print("Inserting network data..")
+        print(server.insert_network(metrics))
+
+
+        # INSERT AVAILABILITY METRICS
+        cluster_id= 1
+        availability_score =0
+        date = '28-10-2022'
+        print("Insert availability data..")
+        metrics =(cluster_id,availability_score,date)
+        print(server.insert_availability(metrics))
+
+        # RETRIEVE all network METRICS
+        print("GET network data..")
+        print(server.select_all_network_metrics())
+
+        # RETRIEVE METRICS per cluster
+        cluster_id = 1
+        metric_type = "availability"
+        print("Retrieve  " +metric_type+ " data for cluster " +str(cluster_id))
+        print(server.select_metrics_by_cluster(cluster_id,metric_type))
+
+    except:
+        print("Error: ", sys.exc_info())
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,164 @@
+from jsonrpcserver import Success, method, serve,  InvalidParams, Result, Error
+from datetime import datetime
+import sqlite3
+from sqlite3 import Error
+import re
+import json
+
+@method
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+@method
+def select_all_network_metrics() -> Result:
+    database = "metrics.db"
+
+    # create a database connection
+    conn = create_connection(database)
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM network")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    if rows:
+        result = { "network data": rows, "message" : "success", "status":201 }
+    else:
+        result = { "network data": Error, "message" : "failed", "status":500 }
+        
+    return Success(result)
+
+
+@method
+def select_metrics_by_cluster(cluster_id,metric_type) -> Result:
+    database = "metrics.db"
+
+    # create a database connection
+    conn = create_connection(database)
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM "+metric_type+" WHERE cluster_id=?", (cluster_id,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+        
+    if rows:
+        result = { "data": rows, "message" : "success", "status":201 }
+    else:
+        result = { "data": Error, "message" : "failed", "status":500 }
+        
+    return Success(result)
+
+
+@method
+def insert_network(metrics) -> Result:
+    database = "metrics.db"
+    # create a database connection
+    conn = create_connection(database)
+    # Use executemany() to insert multiple records at a time
+    query = conn.execute("INSERT INTO network (cluster_id,throughput,latency, jitter, date) VALUES (?,?,?,?,?)",metrics)
+
+    for row in conn.execute('SELECT * FROM network'):
+        print(row)
+    conn.commit()
+
+    if query:
+        return Success(True)
+    else:
+        return Success(False)
+
+@method
+def insert_availability(metrics) -> Result:
+    database = "metrics.db"
+    # create a database connection
+    conn = create_connection(database)
+    # Use executemany() to insert multiple records at a time
+    query = conn.execute("INSERT INTO availability (cluster_id,availability_score,date) VALUES (?,?,?)",metrics)
+
+    for row in conn.execute('SELECT * FROM network'):
+        print(row)
+    conn.commit()
+
+    if query:
+        return Success(True)
+    else:
+        return Success(False)
+
+def create_table(conn):
+    try:   
+        # create network table
+        sqlite_create_network_table_query = '''CREATE TABLE IF NOT EXISTS network (
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    cluster_id INTEGER NOT NULL,
+                                    throughput REAL NOT NULL,
+                                    latency REAL NOT NULL,
+                                    jitter REAL NOT NULL,
+                                    date datetime);'''
+
+        cursor = conn.cursor()
+        print("Successfully Connected to SQLite")
+        cursor.execute(sqlite_create_network_table_query)
+        conn.commit()
+        print("SQLite network table created")
+
+        # create availability table 
+        sqlite_create_availability_table_query = '''CREATE TABLE IF NOT EXISTS availability (
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    cluster_id INTEGER NOT NULL,
+                                    availability_score REAL NOT NULL,
+                                    date datetime);'''
+
+        cursor = conn.cursor()
+        print("Successfully Connected to SQLite")
+        cursor.execute(sqlite_create_availability_table_query)
+        conn.commit()
+        print("SQLite availability table created")
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Error while creating a sqlite table", error)
+    finally:
+        # if conn:
+        #     conn.close()
+        print("sqlite connection is not closed")
+
+
+def main():
+    database = "metrics.db"
+
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        print("first create table")
+        create_table(conn)
+
+
+if __name__ == "__main__":
+    main()
+    serve('localhost', 5141)
