54cdbd7c5e83146671d10e778c60cfba42c41855
Mubangizi <mubangizia22@gmail.com>
2022-12-07 11:48:21 +0300

    Add flask app for central server

---
+++ central-server/scripts/start-prod.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+
+# apply migrations onto db
+python manage.py db upgrade
+
+# start server
+NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn --worker-tmp-dir /dev/shm --workers=4 --bind 0.0.0.0:5000 --timeout 240 server:app
+
---
+++ central-server/scripts/start.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+# apply migrations onto db
+python manage.py db upgrade
+
+# start server
+flask run --host=0.0.0.0 --port=5000
