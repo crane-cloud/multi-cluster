5951fba71edb603ee356ade5b9112bd04a875c5c
Alex Mwotil <mwotila@gmail.com>
2023-05-15 17:13:49 -0700

    eval candidate - 050

--- peerNode/app_noler_rce.py
+++ peerNode/app_noler_rce.py
@@ -361,13 +361,14 @@ class Cluster:
                         if response is not None:
                             print(response)
 
-                            if response["result"]["params"][1] >= leader_p:
-                                print("Leader profile is worse, start election cycle")
-                                with open('/tmp/eval_da.txt', 'a') as fppc:
-                                    fppc.write("lowProfile: {leader} with proposal {proposal} at {ts}\n".format(leader = self.leaderx["leader"], proposal = self.leaderx["proposal_number"], ts=datetime.datetime.now().strftime("%M:%S.%f")[:-2]))
+                            ### Maybe we shouldn't for another election
+                            #if response["result"]["params"][1] >= leader_p:
+                            #    print("Leader profile is worse, start election cycle")
+                            #    with open('/tmp/eval_da.txt', 'a') as fppc:
+                            #        fppc.write("lowProfile: {leader} with proposal {proposal} at {ts}\n".format(leader = self.leaderx["leader"], proposal = self.leaderx["proposal_number"], ts=datetime.datetime.now().strftime("%M:%S.%f")[:-2]))
 
-                                self.proposal_number = self.leaderx["proposal_number"]
-                                await asyncio.wait_for(self.start_election_cycle(), timeout=self.election_timeout)
+                            #    self.proposal_number = self.leaderx["proposal_number"]
+                            #    await asyncio.wait_for(self.start_election_cycle(), timeout=self.election_timeout)
 
                 # We sleep for the poll leader interval & then send another poll
                 time.sleep(self.pollleader_interval)
